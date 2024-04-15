09294038a853f91dec528b9dd63494383152ac91
Dennis Ideler <ideler.dennis@gmail.com>
2024-04-15 20:44:48 +0100

    Use module name in pattern matching
    Saves a few characters which can prevent line breaks and is a bit more
    readable. Using __MODULE__ is more dynamic but it's easy enough to do
    a replace all via the editor in case the module name ever changes.

--- lib/money.ex
+++ lib/money.ex
@@ -18,20 +18,20 @@ defmodule Money do
     do: struct(__MODULE__, amount: amount, currency: currency)
 
   @spec zero?(t) :: boolean()
-  def zero?(%__MODULE__{amount: amount}), do: amount === 0
+  def zero?(%Money{amount: amount}), do: amount === 0
 
   @spec positive?(t) :: boolean()
-  def positive?(%__MODULE__{amount: amount}), do: amount > 0
+  def positive?(%Money{amount: amount}), do: amount > 0
 
   @spec negative?(t) :: boolean()
-  def negative?(%__MODULE__{amount: amount}), do: amount < 0
+  def negative?(%Money{amount: amount}), do: amount < 0
 
   @spec equals?(t, t) :: boolean()
-  def equals?(%__MODULE__{} = m1, %__MODULE__{} = m2), do: m1 === m2
+  def equals?(%Money{} = m1, %Money{} = m2), do: m1 === m2
 
   @spec gt?(t, t) :: boolean()
-  def gt?(%__MODULE__{amount: a1, currency: c}, %__MODULE__{amount: a2, currency: c}), do: a1 > a2
-  def gt?(%__MODULE__{}, %__MODULE__{}), do: false
+  def gt?(%Money{amount: a1, currency: c}, %Money{amount: a2, currency: c}), do: a1 > a2
+  def gt?(%Money{}, %Money{}), do: false
 
   # initialise: new, parse
   # predicates: equals?, zero?, positive?, negative?, gt?, lt? ge?/gte? le?/lte? eq? ne?, compare
