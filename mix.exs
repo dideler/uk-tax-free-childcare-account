2ec50365d62040fc48d484866cfa9136922ed726
Dennis Ideler <ideler.dennis@gmail.com>
2025-03-25 18:12:54 +0000

    Update Erlang/Elixir versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule UkTaxFreeChildcare.MixProject do
     [
       app: :uk_tax_free_childcare,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
